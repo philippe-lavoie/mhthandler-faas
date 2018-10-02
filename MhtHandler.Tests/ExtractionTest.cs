3bdcef2a27ce30d08739698912887643830c6e85
philippe.lavoie <philippe.lavoie@smith.co>
2018-10-02 09:54:37 -0400

    New: first public version.

---
+++ MhtHandler.Tests/ExtractionTest.cs
@@ -0,0 +1,38 @@
+using Function;
+using MIMER.RFC2045;
+using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using Xunit;
+
+namespace MhtHandler.Tests
+{
+    public class ExtractionTest
+    {
+        [Fact]
+        public void ExtractHtml()
+        {
+            var message = File.OpenText("purchase-order.mhtml").ReadToEnd();
+            FunctionHandler handler = new FunctionHandler();
+            var result = handler.Handle(message);
+            Assert.Contains("Purchase Order 16487645-2", result);
+        }
+
+        [Fact]
+        public void DecodeBase64()
+        {
+            var reader = new MailReader();
+
+            MIMER.IEndCriteriaStrategy endofmessage = new BasicEndOfMessageStrategy();
+            var stream = (Stream)File.Open("purchase-order.mhtml", FileMode.Open);
+
+            var message = reader.ReadMimeMessage(ref stream, endofmessage);
+
+            stream.Close();
+
+            Assert.Equal(2, message.Attachments.Count);
+            Assert.Equal("html", message.Attachments[0].SubType);
+        }
+    }
+}
---
+++ mht-handler/FunctionHandler.cs
@@ -0,0 +1,35 @@
+﻿using MIMER.RFC2045;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public string Handle(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "<html><body>Null input</body></html>";
+            }
+
+            var reader = new MailReader();
+            MIMER.IEndCriteriaStrategy endofmessage = new BasicEndOfMessageStrategy();
+            var stream = (Stream)new MemoryStream(Encoding.UTF8.GetBytes(input));
+
+            var message = reader.ReadMimeMessage(ref stream, endofmessage);
+            stream.Close();
+
+            foreach(var attachment in message.Attachments)
+            {
+                if(attachment.Type == "text" && attachment.SubType.StartsWith("htm"))
+                {
+                    return Encoding.UTF8.GetString(attachment.Data);
+                }
+            }
+
+            return "<html><body>There is no HTML message in this message.</body></html>";
+        }
+    }
+}
